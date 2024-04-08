7246125b017926ed52a3c7deb458402f43834afe
Winnie-Irene <i.winnie@alustudent.com>
2024-04-08 03:36:18 -0700

    103

--- python-classes/103-magic_class.py
+++ python-classes/103-magic_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Define a MagicClass that does exactly as the bytecode provided."""
+"""Define a MagicClass matching exactly a bytecode provided by Holberton."""
 
 import math
 
@@ -24,4 +24,4 @@ class MagicClass:
 
     def circumference(self):
         """Return The circumference of the MagicClass."""
-        return (2 * math.pi * self.__radius
+        return (2 * math.pi * self.__radius)
